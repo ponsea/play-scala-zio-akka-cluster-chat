cbfb3bb104cca779bbf23204048f4fb9d5aebf87
ponsea <puodsea@gmail.com>
2020-04-26 07:07:45 +0900

    Add CommentRepository#findById

--- modules/domain/src/main/scala/domain/aggregates/comment/CommentRepository.scala
+++ modules/domain/src/main/scala/domain/aggregates/comment/CommentRepository.scala
@@ -1,7 +1,9 @@
 package domain.aggregates.comment
 
-import zio.UIO
+import zio.{ IO, UIO }
 
 trait CommentRepository {
+  def findById(id: Comment.Id): IO[Unit, Comment]
+
   def save(comment: Comment): UIO[Unit]
 }
