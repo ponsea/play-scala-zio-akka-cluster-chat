8b9e38d33e8321a4c60e2dbf85dc1fa833cae64f
ponsea <puodsea@gmail.com>
2020-04-30 00:21:07 +0900

    Add comments to DomainError

--- modules/domain/src/main/scala/domain/DomainError.scala
+++ modules/domain/src/main/scala/domain/DomainError.scala
@@ -5,6 +5,10 @@ sealed trait DomainError {
   val message: String
 }
 
+// システム上の復帰不可なエラー型は定義しない、例えば
+// `case class SystemError(throwable: Throwable)`
+// こういったエラーは、ZIO値の`Cause`内にある`defects`として表現する
+
 case class ValidationError(resource: String, override val message: String) extends DomainError {
   override val errorCode = "validation-error"
 }
